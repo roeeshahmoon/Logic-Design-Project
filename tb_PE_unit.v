34d0d8b37b8453f3bed47de22e96740df086ad25
roeeshahmoon <roee.s@circ.zone>
2024-01-23 17:08:44 +0200

    first commit

---
+++ tb_PE_unit.v
@@ -0,0 +1,37 @@
+module tb_PE_unit();
+    // Inputs
+    reg [31:0] up_i, left_i;
+    reg clk_i, rst_ni;
+
+    // Outputs
+    wire [31:0] down_o, right_o;
+    wire [63:0] res_o;
+
+    // Instantiate the block module
+    PE_unit PE_test (
+        .up_i(up_i),
+        .left_i(left_i),
+        .clk_i(clk_i),
+        .rst_ni(rst_ni),
+        .down_o(down_o),
+        .right_o(right_o),
+        .res_o(res_o)
+    );
+
+    // Clock generate
+    initial begin
+        clk_i = 0;
+        forever #5 clk_i = ~clk_i;
+    end
+
+    // Initial stimulus
+    initial begin
+        // Initialize inputs
+        up_i = 8'd2; 
+        left_i = 8'd7; 
+        rst_ni = 0;            
+
+        // start stimulate
+        #10 rst_ni = 1;         
+    end
+endmodule
