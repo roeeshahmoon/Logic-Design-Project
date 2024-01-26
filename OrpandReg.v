7da8004a4512d535c1e64f6cb06cb9376108ac74
roeeshahmoon <roee.s@circ.zone>
2024-01-26 12:53:01 +0200

    tb for OprandReg.v

--- OrpandReg.v
+++ OrpandReg.v
@@ -5,21 +5,19 @@ module OperandRegister #(
 )(
   input wire clk_i,          
   input wire rst_ni,          
-  input wire [DATA_WIDTH-1:0] write_data_Mat, // Data input for Matrix
-
-  output reg [DATA_WIDTH-1:0] read_data_Mat, // Read data for Matrix
-
-  input wire [ADDR_WIDTH-1:0] addr_Mat,  // Address input for Matrix-A
-  input wire write_en_Mat    
+  input wire [DATA_WIDTH-1:0] write_data_Mat_i, // Write data input for Matrix
+  input wire [ADDR_WIDTH-1:0] addr_Mat_i,  // Address input for Matrix
+  input wire write_en_Mat_i,              //Enable write
+  output reg [DATA_WIDTH-1:0] read_data_Mat_o // Read data output for Matrix
 );
   reg [DATA_WIDTH-1:0] matrix_Mat [0:MATRIX_SIZE-1];
   integer i;
   // Matrix Read Logic
   always @(posedge clk_i or posedge !rst_ni) begin
     if (!rst_ni) begin
-      read_data_Mat <= 0;
+      read_data_Mat_o <= 0;
     end else begin
-      read_data_Mat <= matrix_Mat[addr_Mat];
+      read_data_Mat_o <= matrix_Mat[addr_Mat_i];
     end
   end
 
@@ -29,8 +27,8 @@ module OperandRegister #(
       for (i = 0; i < MATRIX_SIZE; i = i + 1) begin
         matrix_Mat[i] <= 0;
       end
-    end else if (write_en_Mat) begin
-      matrix_Mat[addr_Mat] <= write_data_Mat;
+    end else if (write_en_Mat_i) begin
+      matrix_Mat[addr_Mat_i] <= write_data_Mat_i;
     end
   end
 
---
+++ tb_OrpandReg.v
@@ -0,0 +1,70 @@
+`include "OrpandReg.v"
+module tb_OperandRegister;
+
+  // Parameters
+  localparam DATA_WIDTH = 32;
+  localparam ADDR_WIDTH = 4;
+  localparam MATRIX_SIZE = 16;
+
+  // Inputs
+  reg clk_i;
+  reg rst_ni;
+  reg [DATA_WIDTH-1:0] write_data_Mat_i;
+  reg [ADDR_WIDTH-1:0] addr_Mat_i;
+  reg write_en_Mat_i;
+
+  // Outputs
+  wire [DATA_WIDTH-1:0] read_data_Mat_o;
+
+  // Instantiate OperandRegister module
+  OperandRegister #(
+    .DATA_WIDTH(DATA_WIDTH),
+    .ADDR_WIDTH(ADDR_WIDTH),
+    .MATRIX_SIZE(MATRIX_SIZE)
+  ) uut (
+    .clk_i(clk_i),
+    .rst_ni(rst_ni),
+    .write_data_Mat_i(write_data_Mat_i),
+    .read_data_Mat_o(read_data_Mat_o),
+    .addr_Mat_i(addr_Mat_i),
+    .write_en_Mat_i(write_en_Mat_i)
+  );
+
+  // Clock generation
+  initial begin
+    clk_i = 0;
+    forever #5 clk_i = ~clk_i;
+  end
+
+  // Test scenario
+  initial begin
+    // Initialize inputs
+    rst_ni = 0;
+    write_data_Mat_i = 32'd0;
+    addr_Mat_i = 4'd0;
+    write_en_Mat_i = 1;
+
+    // Stop reset
+    #10 rst_ni = 1;
+
+    // Write to Matrix
+    #30 addr_Mat_i = 4'd0;
+		write_data_Mat_i = 32'd8;
+		write_en_Mat_i = 1;
+
+    // Read from Matrix
+    #30 addr_Mat_i = 4'd0;
+     write_en_Mat_i = 0;
+
+    // Write to Matrix
+	#30 addr_Mat_i = 4'd2;
+	    write_data_Mat_i = 32'd88;
+	    write_en_Mat_i = 1;
+	
+	//Read from Matrix
+	#30 addr_Mat_i = 4'd2;
+	    write_en_Mat_i = 0;
+	
+  end
+
+endmodule
