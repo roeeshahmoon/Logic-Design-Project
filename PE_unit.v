06a92a6e584d89ee3dd0135aa8bb12f957de55cf
roeeshahmoon <roee.s@circ.zone>
2024-01-30 23:37:41 +0200

    Pertetz and Vaknin siting fix bug

--- PE_unit.v
+++ PE_unit.v
@@ -1,39 +1,32 @@
-//
-// Verilog Module Lab1_lib.PE_UNIT
-//
-// Created:
-//          by - shahm.UNKNOWN (DESKTOP-O037DOV)
-//          at - 16:11:28 30/01/2024
-//
-// using Mentor Graphics HDL Designer(TM) 2021.1 Built on 14 Jan 2021 at 15:11:42
-//
-
 `resetall
 `timescale 1ns/10ps
-
-module PE_UNIT#(parameter DATA_WIDTH = 32)(
+module PE_unit#(parameter DATA_WIDTH = 32)(
 	up_i, left_i, clk_i, rst_ni, down_o, right_o, res_o, carry_o);
 	
 	input [DATA_WIDTH-1:0] up_i, left_i;
 	output reg [DATA_WIDTH-1:0] down_o, right_o;
 	input clk_i, rst_ni;
+	output wire carry_o;
 	output reg [DATA_WIDTH*2 -1:0] res_o;
-	output reg carry_o;
+	wire [DATA_WIDTH*2 -1:0] Mul_w;
+
 	
-	always @(posedge clk_i) begin
+	always @(posedge !rst_ni or posedge clk_i) begin
 		if(!rst_ni) begin
 			res_o <= 0;
 			down_o <= 0;
 			right_o <= 0;
-			carry_o <= 0; 
 		end
 		else begin
-			{carry_o, res_o} <=  res_o + up_i*left_i;
+			res_o <= res_o + Mul_w;
 			right_o <= left_i;
 			down_o <= up_i;
 		end
-		
-	
 	end
+	
+	assign Mul_w = up_i*left_i;
+	assign carry_o = Mul_w[DATA_WIDTH*2 -1] & res_o[DATA_WIDTH*2 -1];
+	
+	
 endmodule
 
--- Systolic_Mul.v
+++ Systolic_Mul.v
@@ -13,33 +13,34 @@ module SYSTOLIC_MUL#(parameter DATA_WIDTH = 32)
 	wire [DATA_WIDTH-1:0] down_o_0, down_o_1, down_o_2, down_o_3, down_o_4, down_o_5, down_o_6, down_o_7, down_o_8, down_o_9, down_o_10, down_o_11, down_o_12, down_o_13, down_o_14, down_o_15;
 	wire [DATA_WIDTH-1:0] right_o_0, right_o_1, right_o_2, right_o_3, right_o_4, right_o_5, right_o_6, right_o_7, right_o_8, right_o_9, right_o_10, right_o_11, right_o_12, right_o_13, right_o_14, right_o_15;
 	wire [DATA_WIDTH-1:0] res_o_0, res_o_1, res_o_2, res_o_3, res_o_4, res_o_5, res_o_6, res_o_7, res_o_8, res_o_9, res_o_10, res_o_11, res_o_12, res_o_13, res_o_14, res_o_15;
-	
+	wire carry_o_0, carry_o_1, carry_o_2, carry_o_3, carry_o_4, carry_o_5, carry_o_6, carry_o_7, carry_o_8, carry_o_9, carry_o_10, carry_o_11, carry_o_12, carry_o_13, carry_o_14, carry_o_15;
+
 	//get input from outside up and left
-	PE_unit PE0 (up_i_0, left_i_0, clk_i, rst_ni, down_o_0, right_o_0, res_o_0);
+	PE_unit PE0 (up_i_0, left_i_0, clk_i, rst_ni, down_o_0, right_o_0, res_o_0, carry_o_0);
 	
 	//get input from outside up and last level
-	PE_unit PE1 (up_i_1, right_o_0, clk_i, rst_ni, down_o_1, right_o_1, res_o_1);
-	PE_unit PE2 (up_i_2, right_o_1, clk_i, rst_ni, down_o_2, right_o_2, res_o_2);
-	PE_unit PE3 (up_i_3, right_o_2, clk_i, rst_ni, down_o_3, right_o_3, res_o_3);
+	PE_unit PE1 (up_i_1, right_o_0, clk_i, rst_ni, down_o_1, right_o_1, res_o_1, carry_o_1);
+	PE_unit PE2 (up_i_2, right_o_1, clk_i, rst_ni, down_o_2, right_o_2, res_o_2, carry_o_2);
+	PE_unit PE3 (up_i_3, right_o_2, clk_i, rst_ni, down_o_3, right_o_3, res_o_3, carry_o_3);
 	
 	//get input from outside left and up level
-	PE_unit PE4 (down_o_0, left_i_4, clk_i, rst_ni, down_o_4, right_o_4, res_o_4);
-	PE_unit PE8 (down_o_4, left_i_8, clk_i, rst_ni, down_o_8, right_o_8, res_o_8);
-	PE_unit PE12 (down_o_8, left_i_12, clk_i, rst_ni, down_o_12, right_o_12, res_o_12);
+	PE_unit PE4 (down_o_0, left_i_4, clk_i, rst_ni, down_o_4, right_o_4, res_o_4, carry_o_4);
+	PE_unit PE8 (down_o_4, left_i_8, clk_i, rst_ni, down_o_8, right_o_8, res_o_8, carry_o_8);
+	PE_unit PE12 (down_o_8, left_i_12, clk_i, rst_ni, down_o_12, right_o_12, res_o_12, carry_o_12);
 	
 	//get input from last level and up level
 	//second row
-	PE_unit PE5 (down_o_1, right_o_4, clk_i, rst_ni, down_o_5, right_o_5, res_o_5);
-	PE_unit PE6 (down_o_2, right_o_5, clk_i, rst_ni, down_o_6, right_o_6, res_o_6);
-	PE_unit PE7 (down_o_3, right_o_6, clk_i, rst_ni, down_o_7, right_o_7, res_o_7);
+	PE_unit PE5 (down_o_1, right_o_4, clk_i, rst_ni, down_o_5, right_o_5, res_o_5, carry_o_5);
+	PE_unit PE6 (down_o_2, right_o_5, clk_i, rst_ni, down_o_6, right_o_6, res_o_6, carry_o_6);
+	PE_unit PE7 (down_o_3, right_o_6, clk_i, rst_ni, down_o_7, right_o_7, res_o_7, carry_o_7);
 	//third row
-	PE_unit PE9 (down_o_5, right_o_8, clk_i, rst_ni, down_o_9, right_o_9, res_o_9);
-	PE_unit PE10 (down_o_6, right_o_9, clk_i, rst_ni, down_o_10, right_o_10, res_o_10);
-	PE_unit PE11 (down_o_7, right_o_10, clk_i, rst_ni, down_o_11, right_o_11, res_o_11);
+	PE_unit PE9 (down_o_5, right_o_8, clk_i, rst_ni, down_o_9, right_o_9, res_o_9, carry_o_9);
+	PE_unit PE10 (down_o_6, right_o_9, clk_i, rst_ni, down_o_10, right_o_10, res_o_10, carry_o_10);
+	PE_unit PE11 (down_o_7, right_o_10, clk_i, rst_ni, down_o_11, right_o_11, res_o_11, carry_o_11);
 	//fourth row
-	PE_unit PE13 (down_o_9, right_o_12, clk_i, rst_ni, down_o_13, right_o_13, res_o_13);
-	PE_unit PE14 (down_o_10, right_o_13, clk_i, rst_ni, down_o_14, right_o_14, res_o_14);
-	PE_unit PE15 (down_o_11, right_o_14, clk_i, rst_ni, down_o_15, right_o_15, res_o_15);
+	PE_unit PE13 (down_o_9, right_o_12, clk_i, rst_ni, down_o_13, right_o_13, res_o_13, carry_o_13);
+	PE_unit PE14 (down_o_10, right_o_13, clk_i, rst_ni, down_o_14, right_o_14, res_o_14, carry_o_14);
+	PE_unit PE15 (down_o_11, right_o_14, clk_i, rst_ni, down_o_15, right_o_15, res_o_15, carry_o_15);
 	
 	always @(posedge clk_i or posedge !rst_ni) begin
 		if(!rst_ni) begin
